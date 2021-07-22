1232cbc69bf4ee94fb9e288d0fbca5b5f548552f
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-21 23:45:51 -0400

    made it

--- YassaAlchemy/__init__.py
+++ YassaAlchemy/__init__.py
@@ -17,6 +17,29 @@ class setup:
 
 class table(setup):
     def __init__(self,host,user,passwd,database):
+        self.table = ""
         super().__init__(host,user,passwd,database)
+    
+    def mk_table(self,table_name):
+
+        self.table = "CREATE TABLE IF NOT EXISTS "+table_name+"( "
+
+        return "table is added"
+    
+    def mk_column(self,column_name,type):
+        self.table+=column_name+" "+type+","
+        return "added_table"
+    def publish_db(self):
+        connect = setup(self.host,self.user,self.passwd,self.database)
+        mydb = connect.connect()
+        mycursor = mydb.cursor()
+        self.table=self.table[0:-1]
+        self.table+=")"
+        #self.table[-2]="a"
+        print(self.table)
+        mycursor.execute(self.table)
+        mycursor.close()        
+        return "succesfully created the table"
+
 
 
--- test.py
+++ test.py
@@ -1,7 +1,22 @@
+#this is a test page
+
 import YassaAlchemy
 
 
 db = YassaAlchemy.table(host="localhost",user="root",passwd="new_password",database="yassadb")
-db.connect()
 
 
+
+
+class Users:
+    def create(db):
+        db.connect()
+        #table_name =
+        #name = db.table()
+        db.mk_table("Users")
+        db.mk_column("Username","VARCHAR(500)")
+        db.mk_column("Password","VARCHAR(500)")
+        db.publish_db()
+    
+
+Users.create(db)
