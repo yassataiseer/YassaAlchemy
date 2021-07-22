071e9f30df3922a88856e482e889ba3f926dc2c6
Yassa Taiseer <yassataiseer@gmail.com>
2021-07-21 23:53:14 -0400

    perfected docs

--- test.py
+++ test.py
@@ -1,22 +1,27 @@
 #this is a test page
 
-import YassaAlchemy
+import YassaAlchemy #import library
 
 
 db = YassaAlchemy.table(host="localhost",user="root",passwd="new_password",database="yassadb")
-
+#create instance of YassaAlchemy
+#make sure a database is created and insert
+#the neccesary credentials
 
 
 
 class Users:
+    #make static class
+
+
+    #class for Users
     def create(db):
         db.connect()
-        #table_name =
-        #name = db.table()
-        db.mk_table("Users")
-        db.mk_column("Username","VARCHAR(500)")
-        db.mk_column("Password","VARCHAR(500)")
-        db.publish_db()
+        #connect to db
+        db.mk_table("Users")#name the table
+        db.mk_column("Username","VARCHAR(500)")#column one(Username) and type of data(Varchar)
+        db.mk_column("Password","VARCHAR(500)")#column two(Password) and type of data(Varchar)
+        db.publish_db()#commit changes
     
 
-Users.create(db)
+Users.create(db) #create User's table
